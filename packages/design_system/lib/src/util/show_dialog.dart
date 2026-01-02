2ae7b313eaa802707359ca5415af04ae74601811
hom237 <highcleaner@naver.com>
2026-01-02 13:14:34 +0900

    feat:: showDialog 메서드 배경색 변경

--- packages/design_system/lib/src/util/show_dialog.dart
+++ packages/design_system/lib/src/util/show_dialog.dart
@@ -9,12 +9,20 @@ Future<T?> showDialog<T>({
 }) async {
   return await m.showDialog<T>(
     context: context,
+    barrierColor: Colors.white.withOpacity(0.3),
     builder: (context) {
       return m.Dialog(
         backgroundColor: Colors.transparent,
         shape: m.RoundedRectangleBorder(borderRadius: BorderRadius.all.normal),
-        insetPadding:m. EdgeInsets.all(m.MediaQuery.of(context).size.width / 18),
-        child: Container(width: double.infinity, child: builder(context),),
+        insetPadding: m.EdgeInsets.all(
+          m.MediaQuery.of(context).size.width / 24,
+        ),
+        child: Container(
+          width: double.infinity,
+          child: m.SingleChildScrollView(
+              padding: m.EdgeInsets.all(20),
+              child: builder(context)),
+        ),
       );
     },
   );
