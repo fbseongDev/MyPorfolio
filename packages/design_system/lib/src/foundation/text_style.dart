aeced35f14987fa952f309760f9748ea4e38621d
hom237 <highcleaner@naver.com>
2026-01-07 13:56:51 +0900

    chore:: text_style 기본값 제거

--- packages/design_system/lib/src/foundation/text_style.dart
+++ packages/design_system/lib/src/foundation/text_style.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart' as m;
-import 'package:design_system/src/foundation/colors.dart';
-import 'package:design_system/src/foundation/font_weight.dart';
 
 @m.immutable
 final class TextStyle extends m.TextStyle {
   const TextStyle({
-    super.fontSize = 14,
-    super.fontWeight = FontWeight.normal,
-    super.color= Colors.white,
-    super.height = 1,
+    super.fontSize,
+    super.fontWeight,
+    super.color,
+    super.height,
     super.letterSpacing,
-    super.decoration = m.TextDecoration.none
+    super.decoration
   });
 }
