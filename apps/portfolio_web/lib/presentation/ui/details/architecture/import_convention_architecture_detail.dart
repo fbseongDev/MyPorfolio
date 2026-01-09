131acf3c81aacadfb0e9e8dd19a8c4745cb5083c
hom237 <highcleaner@naver.com>
2026-01-09 11:03:33 +0900

    chore:: import_convention_architecture_detail 모듈화 - preview 추가

--- apps/portfolio_web/lib/presentation/ui/details/architecture/import_convention_architecture_detail.dart
+++ apps/portfolio_web/lib/presentation/ui/details/architecture/import_convention_architecture_detail.dart
@@ -1,99 +1,163 @@
 import 'package:convention/ui.dart';
 
 class ImportConventionArchitectureDetail extends HookWidget {
+  @Preview(group: 'ImportConventionArchitectureDetail', name: 'Detail')
   const ImportConventionArchitectureDetail({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 32,
       children: [
-        Text(
-          '필수 사항',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(height: 12),
-        Container(
-          padding: EdgeInsets.all(16),
-          child: Text('Monorepo, Clean Architecture, Design System'),
-        ),
-        SizedBox(height: 32),
-        Text('목적', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-        SizedBox(height: 12),
-        Container(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: 16,
-            children: [
-              Text('1. 로직 구현 시 외부 라이브러리 선택 범위 제한'),
-              Text('2. 표준 인터페이스의 파라미터 및 속성 제한을 통한 사용법 통제'),
-              Text('3. 팀 공통 규칙과 디자인 시스템을 기본으로 적용하면서 사용법은 기존 인터페이스 사용법 유지'),
-              Text('4. 신규 팀원의 빠른 적응과 일관된 코드 작성으로 DX 개선'),
-              Text('5. 멤버 사용처 추적 및 관리 용이성 향상'),
-            ],
-          ),
-        ),
-        SizedBox(height: 32),
-        Text('규칙', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-        SizedBox(height: 12),
-        Container(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: 16,
-            children: [
-              Text(
-                '1. Monorepo와 Clean Architecture를 기반으로, 실행 환경과 직접 연결되는 모듈(Runtime Layer)과 팀 공통 규칙을 적용할 모듈(Convention 모듈)을 명확히 구분한다.',
-              ),
-              Text(
-                '2. 실행 환경 모듈(Runtime Layer)에 의존하는 모듈은 Convention 모듈에 의존하지 않아야 한다.',
-              ),
-              Text('3. Convention 모듈이 의존하는 모듈은 Convention 모듈에 의존하지 않아야 한다.'),
-              Text('4. Convention 모듈을 의존하는 모듈은 외부 라이브러리에 의존하지 않아야 한다.'),
-              Text(
-                '5. Convention 모듈은 불필요한 라이브러리에 절대 의존하지 않아야 하며, 필요한 라이브러리도 제한된 멤버만 접근 가능하도록 해야 한다.',
-              ),
-              Text('6. Convention 모듈은 레이어별로 라이브러리 파일을 구분하여 생성한다.'),
-              Text(
-                '7. UI 요소, 기능 함수, 메서드, 설정 객체 등 모든 멤버는 단일 책임 원칙에 따라 동일한 이름과 사용 방식을 유지한 별도 멤버로 매핑하여 사용자가 혼동 없이 동일한 방법으로 접근할 수 있도록 한다.',
-              ),
-              Text(
-                '※ Convention 모듈에서 제공되는 모든 표준 인터페이스(Standard API)는 내부적으로 팀 공통 규칙과 디자인 시스템을 적용하도록 구현되어 있으며, 개발자는 기존 사용법 그대로 사용하기만 하면 자동으로 정책과 스타일이 적용된다.',
-              ),
-            ],
-          ),
-        ),
+        _requiredBuilder(),
+        _purposeBuilder(),
+        _ruleBuilder(),
+        _weaknessBuilder(),
+        _exampleBuilder(),
+      ],
+    );
+  }
 
-        SizedBox(height: 32),
-        Text('단점', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-        SizedBox(height: 12),
-        Container(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: 16,
-            children: [
-              Text('1. Convention으로 구현된 표준 인터페이스는 기본 사용법으로도 구현이 가능하도록 설계해야 함'),
-              Text('2. 아키텍쳐를 처음 접했을 때 표준 인터페이스의 지식이 높을수록 혼란을 겪을 수 있음'),
-            ],
+  Widget _requiredBuilder() => Builder(
+    builder: (context) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '필수 사항',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
-        ),
+          SizedBox(height: 12),
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Text('Monorepo, Clean Architecture, Design System'),
+          ),
+        ],
+      );
+    },
+  );
 
-        SizedBox(height: 32),
-        Text('예시', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-        SizedBox(height: 12),
-        Container(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '기존 코드',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-              ),
-              SizedBox(height: 16),
-              Code('''
+  Widget _purposeBuilder() => Builder(
+    builder: (context) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '목적',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 12),
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 16,
+              children: [
+                Text('1. 로직 구현 시 외부 라이브러리 선택 범위 제한'),
+                Text('2. 표준 인터페이스의 파라미터 및 속성 제한을 통한 사용법 통제'),
+                Text('3. 팀 공통 규칙과 디자인 시스템을 기본으로 적용하면서 사용법은 기존 인터페이스 사용법 유지'),
+                Text('4. 신규 팀원의 빠른 적응과 일관된 코드 작성으로 DX 개선'),
+                Text('5. 멤버 사용처 추적 및 관리 용이성 향상'),
+              ],
+            ),
+          ),
+        ],
+      );
+    },
+  );
+
+  Widget _ruleBuilder() => Builder(
+    builder: (context) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '규칙',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 12),
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 16,
+              children: [
+                Text(
+                  '1. Monorepo와 Clean Architecture를 기반으로, 실행 환경과 직접 연결되는 모듈(Runtime Layer)과 팀 공통 규칙을 적용할 모듈(Convention 모듈)을 명확히 구분한다.',
+                ),
+                Text(
+                  '2. 실행 환경 모듈(Runtime Layer)에 의존하는 모듈은 Convention 모듈에 의존하지 않아야 한다.',
+                ),
+                Text('3. Convention 모듈이 의존하는 모듈은 Convention 모듈에 의존하지 않아야 한다.'),
+                Text('4. Convention 모듈을 의존하는 모듈은 외부 라이브러리에 의존하지 않아야 한다.'),
+                Text(
+                  '5. Convention 모듈은 불필요한 라이브러리에 절대 의존하지 않아야 하며, 필요한 라이브러리도 제한된 멤버만 접근 가능하도록 해야 한다.',
+                ),
+                Text('6. Convention 모듈은 레이어별로 라이브러리 파일을 구분하여 생성한다.'),
+                Text(
+                  '7. UI 요소, 기능 함수, 메서드, 설정 객체 등 모든 멤버는 단일 책임 원칙에 따라 동일한 이름과 사용 방식을 유지한 별도 멤버로 매핑하여 사용자가 혼동 없이 동일한 방법으로 접근할 수 있도록 한다.',
+                ),
+                Text(
+                  '※ Convention 모듈에서 제공되는 모든 표준 인터페이스(Standard API)는 내부적으로 팀 공통 규칙과 디자인 시스템을 적용하도록 구현되어 있으며, 개발자는 기존 사용법 그대로 사용하기만 하면 자동으로 정책과 스타일이 적용된다.',
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
+    },
+  );
+
+  Widget _weaknessBuilder() => Builder(
+    builder: (context) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '단점',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 12),
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 16,
+              children: [
+                Text(
+                  '1. Convention으로 구현된 표준 인터페이스는 기본 사용법으로도 구현이 가능하도록 설계해야 함',
+                ),
+                Text('2. 아키텍쳐를 처음 접했을 때 표준 인터페이스의 지식이 높을수록 혼란을 겪을 수 있음'),
+              ],
+            ),
+          ),
+        ],
+      );
+    },
+  );
+
+  Widget _exampleBuilder() => Builder(
+    builder: (context) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '예시',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 12),
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '기존 코드',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                SizedBox(height: 16),
+                Code('''
 import 'package:flutter/material.dart';
 
 final widget = ClipRRect(
@@ -112,13 +176,13 @@ final widget = ClipRRect(
     ),
   ),
 );''', language: CodeLanguage.dart),
-              SizedBox(height: 24),
-              Text(
-                'Import Convention Architecture 적용 코드',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-              ),
-              SizedBox(height: 16),
-              Code('''
+                SizedBox(height: 24),
+                Text(
+                  'Import Convention Architecture 적용 코드',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                SizedBox(height: 16),
+                Code('''
 // export 'package:flutter/material.dart' show Text;
 // export 'package:design_system/design_system' show Container;
 import 'package:convention/ui.dart';
@@ -128,73 +192,75 @@ final widget = Container(
   height: 30,
   child: Text('Hello, world!'),
 );''', language: CodeLanguage.dart),
-              SizedBox(height: 24),
-              Text(
-                '예시 패키지 구조',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-              ),
-              SizedBox(height: 16),
-              Package(
-                node: PackageNode(
-                  name: 'MyPortfolio',
-                  children: [
-                    PackageNode(
-                      name: 'apps',
-                      children: [
-                        PackageNode(
-                          name: 'portfolio_web',
-                          children: [PackageNode(name: 'main.dart')],
-                        ),
-                      ],
-                    ),
-                    PackageNode(
-                      name: 'packages',
-                      children: [
-                        PackageNode(
-                          name: 'convention',
-                          children: [
-                            PackageNode(name: 'app.dart'),
-                            PackageNode(name: 'ui.dart'),
-                          ],
-                          comment: 'design_system을 의존'
-                        ),
-                        PackageNode(
-                          name: 'design_system',
-                          children: [
-                            PackageNode(name: 'src', children: [
-                              PackageNode(name: 'container.dart')
-                            ]),
-                            PackageNode(name: 'design_system.dart'),
-                          ],
-                        ),
-                      ],
-                    ),
-                    PackageNode(name: 'pubspecs.yaml'),
-                  ],
+                SizedBox(height: 24),
+                Text(
+                  '예시 패키지 구조',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
                 ),
-              ),
-              SizedBox(height: 16),
-              Text.rich(
-                TextSpan(
-                  style: TextStyle(fontSize: 13, color: Colors.white),
-                  children: [
-                    TextSpan(
-                      text: '실제 MyPortfolio 프로젝트의 패키지 구조 중 일부이며 자세한 코드는 ',
-                    ),
+                SizedBox(height: 16),
+                Package(
+                  node: PackageNode(
+                    name: 'MyPortfolio',
+                    children: [
+                      PackageNode(
+                        name: 'apps',
+                        children: [
+                          PackageNode(
+                            name: 'portfolio_web',
+                            children: [PackageNode(name: 'main.dart')],
+                          ),
+                        ],
+                      ),
+                      PackageNode(
+                        name: 'packages',
+                        children: [
+                          PackageNode(
+                            name: 'convention',
+                            children: [
+                              PackageNode(name: 'app.dart'),
+                              PackageNode(name: 'ui.dart'),
+                            ],
+                            comment: 'design_system을 의존',
+                          ),
+                          PackageNode(
+                            name: 'design_system',
+                            children: [
+                              PackageNode(
+                                name: 'src',
+                                children: [PackageNode(name: 'container.dart')],
+                              ),
+                              PackageNode(name: 'design_system.dart'),
+                            ],
+                          ),
+                        ],
+                      ),
+                      PackageNode(name: 'pubspecs.yaml'),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 16),
+                Text.rich(
+                  TextSpan(
+                    style: TextStyle(fontSize: 13, color: Colors.white),
+                    children: [
+                      TextSpan(
+                        text: '실제 MyPortfolio 프로젝트의 패키지 구조 중 일부이며 자세한 코드는 ',
+                      ),
 
-                    TextSpan(
-                      text: 'https://github.com/fbseongDev/MyPorfolio',
-                      style: TextStyle(decoration: TextDecoration.underline),
-                      type: TextSpanType.hyperLink
-                    ),
-                    TextSpan(text: '를 참고해주세요'),
-                  ],
+                      TextSpan(
+                        text: 'https://github.com/fbseongDev/MyPorfolio',
+                        style: TextStyle(decoration: TextDecoration.underline),
+                        type: TextSpanType.hyperLink,
+                      ),
+                      TextSpan(text: '를 참고해주세요'),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
-    );
-  }
+        ],
+      );
+    },
+  );
 }
